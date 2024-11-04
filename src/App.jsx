1d7615f8b9ead3226243e509adf30132cc565583
Tuan Pham <rongcon963@yahoo.com>
2024-11-04 16:51:29 +0700

    React router

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 // import './App.css';
+import Blog from '@components/Blog/Blog';
 import HomePage from '@components/HomePage/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import routers from '@/routers/routers';
+import { Suspense } from 'react';
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {routers.map((item, index) => {
+            return (
+              <Route
+                key={index}
+                path={item.path}
+                element={<item.component />}
+              />
+            );
+          })}
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
