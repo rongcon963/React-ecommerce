916f3d16ef8de71de4adf337fea199fa3994a682
Tuan Pham <rongcon963@yahoo.com>
2025-01-23 13:33:24 +0700

    Add toastify and call api login

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,31 @@ import routers from '@/routers/routers';
 import { Suspense } from 'react';
 import { SideBarProvider } from '@/contexts/SideBarProvider';
 import Sidebar from '@components/Sidebar/Sidebar';
+import { ToastProvider } from '@/contexts/ToastProvider';
 
 function App() {
   return (
-    <SideBarProvider>
-      <Sidebar />
+    <ToastProvider>
+      <SideBarProvider>
+        <Sidebar />
 
-      <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            {routers.map((item, index) => {
-              return (
-                <Route
-                  key={index}
-                  path={item.path}
-                  element={<item.component />}
-                />
-              );
-            })}
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </SideBarProvider>
+        <BrowserRouter>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              {routers.map((item, index) => {
+                return (
+                  <Route
+                    key={index}
+                    path={item.path}
+                    element={<item.component />}
+                  />
+                );
+              })}
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </SideBarProvider>
+    </ToastProvider>
   );
 }
 
--- src/components/ContentSidebar/Login/Login.jsx
+++ src/components/ContentSidebar/Login/Login.jsx
@@ -3,11 +3,15 @@ import styles from './styles.module.scss';
 import Button from '@components/Button/Button';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { ToastContext } from '@/contexts/ToastProvider';
+import { register } from '@/apis/authService';
 
 function Login() {
   const { container, title, boxRememberMe, lostPw } = styles;
   const [isRegister, setIsRegister] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useContext(ToastContext);
 
   const formik = useFormik({
     initialValues: {
@@ -24,8 +28,23 @@ function Login() {
         .required('Confirm password is required')
         .oneOf([Yup.ref('password'), null], 'Passwords must match')
     }),
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: async (values) => {
+      if (isLoading) return;
+
+      if (isRegister) {
+        const { email: username, password } = values;
+
+        setIsLoading(true);
+        await register({ username, password })
+          .then((res) => {
+            toast.success(res.data.message);
+            setIsLoading(false);
+          })
+          .catch((error) => {
+            toast.error(error.response.data.message);
+            setIsLoading(false);
+          });
+      }
     }
   });
 
@@ -70,7 +89,10 @@ function Login() {
           </div>
         )}
 
-        <Button content={isRegister ? 'REGISTER' : 'LOGIN'} type='submit' />
+        <Button
+          content={isLoading ? 'Loading...' : isRegister ? 'REGISTER' : 'LOGIN'}
+          type='submit'
+        />
       </form>
       <Button
         content={
